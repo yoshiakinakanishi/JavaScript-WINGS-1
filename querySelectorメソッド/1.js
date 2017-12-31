d805aeac5e8a2f3818c07c72fd5f90f0c2e5deca
kiryu kazuma <nakanishiyoshiaki0915@gmail.com>
2018-01-01 04:12:33 +0900

    Add files via upload

---
+++ querySelectorメソッド/1.js
@@ -0,0 +1,10 @@
+
+var list = document.querySelectorAll('#hero .ex');
+for(var i = 0; i < list.length; i++) {
+    console.log(list.item(i).href);
+}
+
+/*
+https://www.amazon.co.jp
+https://www.rakuten.co.jp/
+*/
