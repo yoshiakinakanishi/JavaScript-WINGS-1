245f677640ade41c416d1c23f50d13a05beecc2d
kiryu kazuma <nakanishiyoshiaki0915@gmail.com>
2018-01-01 03:50:17 +0900

    Add files via upload

--- getElementsByTagとgetElementsByClassNameメソッド/1.js
+++ getElementsByTagとgetElementsByClassNameメソッド/1.js
@@ -4,7 +4,19 @@ for(var i = 0; i < list.length; i++) {
     console.log(list.item(i).href);
 };
 
+/*
+https://www.google.com
+https://www.google.co.jp
+https://www.dmm.com
+https://www.dmm.co.jp
+*/
+
 var my = document.getElementsByClassName('my');
 for(var i = 0; i < my.length; i++) {
     console.log(my.item(i).href);
 }
+
+/*
+https://www.dmm.com
+https://www.dmm.co.jp
+*/
